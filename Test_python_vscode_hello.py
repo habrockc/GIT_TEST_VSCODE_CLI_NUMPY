562712fac1abad2c95ba2c6a0ea48f2e1df65eaa
habrockc <habrockc@aol.com>
2021-10-08 11:33:18 -0500

    initial test code import pandas import numpy

---
+++ Test_python_vscode_hello.py
@@ -0,0 +1,18 @@
+print("Testing", "\n"*4)
+print("it worked")
+
+print()
+print()
+print("Now trying to import Pnadas")
+import pandas as pd
+import numpy as np
+
+start = 1
+stop = 200
+step = 33
+
+myRange = np.arange(start, stop, step)
+print(f"My np-created range: {myRange}","\n"*2)
+
+print()
+print("Pandas was installed, great!")
